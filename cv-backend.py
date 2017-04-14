0f985f93adc72e47a24f9b7a830cba5429c5fe3b
Flavio Ferrara <thecrow.eric@gmail.com>
2017-04-14 17:00:38 -0300

    Fix encoding

--- cv-backend.py
+++ cv-backend.py
@@ -4,7 +4,7 @@ from ws.ws import WebSocketServer
 import rx
 
 if __name__ == '__main__':
-    with open('res/cv.json') as f:
+    with open('res/cv.json', encoding='utf-8') as f:
         conversation_json = f.read()
 
     wss = WebSocketServer(conversation_json)
